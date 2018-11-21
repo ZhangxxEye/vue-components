c4aa61c5d38c04481958fe2ccd1b969464c1a439
zhangxiaoxia <zhangxiaoxia@51talk.com>
2018-11-21 18:47:51 +0800

    搜索组件

---
+++ src/components/search/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="search-field flex-box">
+        <i class="search-icon"></i>
+        <div class="search-input flex-box flex-item">
+            <input type="search" :value="value" @focus="focus" @input="change" :placeholder="placeholder" class="input-field" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  methods: {
+    focus () {
+      this.$emit('focus')
+    },
+    change($event) {
+      this.$emit('input', $event.target.value)
+    }
+  },
+  props: {
+      placeholder: {
+        type: String,
+        default: '请输入'
+      },
+      value: String
+  }
+};
+</script>
+<style lang="less" scoped>
+.search-field {
+  width: 100%;
+  background: rgba(242, 242, 242, 1);
+  border-radius: 34px;
+  padding: 8px 16px;
+  // padding: 0px 16px;
+  box-sizing: border-box;
+  .search-icon {
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+    background: url("./img/search.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .search-input{
+    height: 48px;
+    align-items: center;
+  }
+  .input-field {
+    height: 100%;
+    width: 100%;
+    font-size: 24px;
+    font-weight: 400;
+    color: rgba(153, 153, 153, 1);
+    // padding-top: 3px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,42 @@
 <template>
-  <div class="greeter"> {{ greeter }}</div>
+  <div>
+
+    <div class="greeter"> {{ greeter }}</div>
+    <search v-model="searchVal"></search>
+  </div>
+
 </template>
 
 <script>
-  export default {
-    name: 'home',
-    data() {
-      return {
-        greeter: 'Hello, Vue!'
-      }
-    },
-    created() {
-      console.log('发送请求4444444444444')
-      this.$http.getApiXxx().then((response) => {
+import search from "@/components/search";
+export default {
+  name: "home",
+  data() {
+    return {
+      greeter: "Hello, Vue!",
+      searchVal: ''
+    };
+  },
+  components: {
+    search
+  },
+  watch: {
+    searchVal (val) {
+      console.log(val)
+    }
+  },
+  created() {
+    console.log("发送请求4444444444444");
+    this.$http
+      .getApiXxx()
+      .then(response => {
         console.log(response);
-      }).catch((response) => {
+      })
+      .catch(response => {
         console.log(response);
       });
-    }
   }
+};
 </script>
 
 <style lang="less" src="./index.less"></style>
